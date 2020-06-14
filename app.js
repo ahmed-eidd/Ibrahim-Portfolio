0eeb6a5342a227974ed658c868aa8719ba7f3338
Ahmed Eid <ahmedeid.webdev@gmail.com>
2020-06-14 09:57:15 +0200

    adding photo for the gallery section

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const scroll = new SmoothScroll('a[href*="#"]');
 
 const nav = document.querySelector('.nav');
-
+const logo = document.querySelector('.nav__logo')
 window.addEventListener('scroll', () => {
   nav.classList.toggle('sticky', window.scrollY > 0) 
+  logo.classList.toggle('sticky-logo', window.scrollY > 0)
 })
