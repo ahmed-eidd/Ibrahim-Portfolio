2de409e81342323f46859b3e280b6ccf5a2d82ee
Ahmed Eid <ahmedeid.webdev@gmail.com>
2020-06-17 09:33:40 +0200

    fixing nav doesn't appear onreload half page

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const scroll = new SmoothScroll('a[href*="#"]');
 
 const nav = document.querySelector('.nav');
-const logo = document.querySelector('.nav__logo')
+const logo = document.querySelector('.nav__logo');
 window.addEventListener('scroll', () => {
-  nav.classList.toggle('sticky', window.scrollY > 0) 
-  logo.classList.toggle('sticky-logo', window.scrollY > 0)
-})
+  logo.classList.toggle('sticky-logo', window.scrollY > 0);
+  nav.classList.toggle('sticky', window.scrollY > 0);
+});
+logo.classList.toggle('sticky-logo', window.pageYOffset > 0);
+nav.classList.toggle('sticky', window.pageYOffset > 0);
+
+
+// const navList = document.querySelector('.nav__list')
+// const navLinks = document.querySelectorAll('.nav__items');
+
+// for (let i = 0; i < navLinks.length; i++) {
+//   navLinks[i].addEventListener('click', () => {
+//     var current = document.getElementsByClassName('active')
+//     current[0].className - current[0].className.replace(' active', '')
+//     this.classList += 'active';
+//   });
+// }
+
+// // Add active class to the current button (highlight it)
+// // var header = document.getElementById("myDIV");
+// // var btns = header.getElementsByClassName("btn");
+// // for (var i = 0; i < btns.length; i++) {
+// //   btns[i].addEventListener("click", function() {
+// //   var current = document.getElementsByClassName("active");
+// //   current[0].className = current[0].className.replace(" active", "");
+// //   this.className += " active";
+// //   });
+// // }
